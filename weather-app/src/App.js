6b0f723541741c5fe32e690577de577c58faa597
FrankoM85 <frankmoln85@gmail.com>
2021-08-05 10:37:32 +0200

    adding my files

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import './App.css';
+import Citys from './components/citys'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [city, setCity] = useState('Budapest');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${(city === null ? 'Budapest' : city)}&appid=6f79047060b4fdc997a285c479048a67`)
+      .then(res => res.json())
+      .then(
+        (items) => {
+          setItems(items);
+        }
+      )
+    }, [city])
+
+  if (items.main !== undefined) {
+
+return (
+  <div>
+    <h1>Weather</h1>
+    <Autocomplete
+    value={city}
+    onChange={(event, newValue) => {
+      setCity(newValue);
+      }}
+    id="combo-box-demo"
+    className="Combobox"
+    options={Citys}
+    style={{ width: 300 }}
+    renderInput={(params) => <TextField {...params} variant="outlined" />}
+    />
+    <table>
+      <tr>
+        <th>Humidity (RH)</th>
+        <th>Temperature</th>
+        <th>Sky Condition</th>
+      </tr>
+      <tr>
+      <td>{items.main.humidity} %</td>
+        <td>{(items.main.temp -273.15).toFixed(1)} °C</td>
+        <td>{items.weather[0].description}</td> 
+      </tr> 
+      </table>
+  </div>
+    );
+  } else { return `Loading...`}
 }
 
 export default App;
+
